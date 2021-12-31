e30ab2db96e65a09eae8a8732a0483793c9f5c97
Tomasz Chlebek <tomasz.chlebek@issp.pl>
2021-12-31 18:45:55 +0100

    new url

--- polyfills.js
+++ polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\tomas\Documents\Development\Moje\tomaszchlebek.dev\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! G:\Development\Moje\tomaszchlebek.dev\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
